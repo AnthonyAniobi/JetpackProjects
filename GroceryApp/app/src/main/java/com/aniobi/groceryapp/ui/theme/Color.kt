2c1c9aacb4db4a87832fe62fac6c3e6e485472c9
Anthony Aniobi <anthonyaniobi198@gmail.com>
2023-03-30 20:54:50 +0100

    added grocery app

---
+++ GroceryApp/app/src/main/java/com/aniobi/groceryapp/ui/theme/Color.kt
@@ -0,0 +1,5 @@
+package com.aniobi.groceryapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val orange = Color(0xFFF2994A)
---
+++ GroceryApp/app/src/main/java/com/aniobi/groceryapp/ui/theme/Type.kt
@@ -0,0 +1,32 @@
+package com.aniobi.groceryapp.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.aniobi.groceryapp.R
+
+
+val nunitoFamily = FontFamily(
+    Font(R.font.nunito_light, FontWeight.Light),
+    Font(R.font.nunito_regular, FontWeight.Normal),
+    Font(R.font.nunito_bold, FontWeight.Bold)
+)
+// Set of Material typography styles to start with
+val Typography = Typography(
+    defaultFontFamily = nunitoFamily
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+)
