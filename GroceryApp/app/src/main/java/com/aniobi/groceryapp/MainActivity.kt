ff6b451b5916b47aae4bf55b07d198d1f5f6b7d1
Anthony Aniobi <anthonyaniobi198@gmail.com>
2023-03-31 02:02:09 +0100

    create homepage scaffold

--- GroceryApp/app/src/main/java/com/aniobi/groceryapp/MainActivity.kt
+++ GroceryApp/app/src/main/java/com/aniobi/groceryapp/MainActivity.kt
@@ -3,41 +3,129 @@ package com.aniobi.groceryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aniobi.groceryapp.ui.theme.GroceryAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GroceryAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MainTheme {
+                HomeScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainTheme(content :@Composable ()->Unit){
+    GroceryAppTheme {
+        Surface(color = MaterialTheme.colors.primary) {
+            content()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    GroceryAppTheme {
-        Greeting("Android")
+    MainTheme {
+        HomeScreen()
+    }
+}
+
+@Composable
+fun HomeScreen(){
+    Box(modifier=Modifier.background(color = Color.White)){
+        Image(
+            modifier= Modifier.fillMaxWidth().offset(0.dp, (-30).dp),
+            painter = painterResource(id = R.drawable.bg_main),
+            contentDescription = "Header Background",
+            contentScale = ContentScale.FillWidth
+        )
+        Scaffold(
+            topBar = { AppBar() },
+            backgroundColor = Color.Transparent
+        ) { paddingValues ->
+            Content(paddingValues)
+        }
+    }
+}
+
+@Composable
+fun AppBar(){
+
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
+            .height(48.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+    ){
+        TextField(
+            value = "",
+            label = {Text("Search Food, Vegetable, etc", maxLines = 1)},
+            onValueChange = {},
+            leadingIcon = { Icon(imageVector = Icons.Rounded.Search, contentDescription = "Search")},
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            ),
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        )
+        Spacer(modifier=Modifier.width(8.dp))
+        IconButton(onClick = { }) {
+            Icon(imageVector = Icons.Outlined.FavoriteBorder, contentDescription = "", tint=Color.White)
+        }
+        IconButton(onClick = { }) {
+            Icon(imageVector = Icons.Outlined.Notifications, contentDescription = "", tint=Color.White)
+        }
+
+    }
+}
+
+@Composable
+fun Content(paddingValues: PaddingValues){
+    Column(modifier= Modifier.padding(paddingValues)) {
+        Header()
+    }
+}
+
+@Composable
+fun Header(){
+    Card(modifier = Modifier
+        .height(64.dp)
+        .padding(horizontal=16.dp),
+        elevation = 4.dp,
+        shape = RoundedCornerShape(8.dp)
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+        }
     }
 }
--- GroceryApp/app/src/main/java/com/aniobi/groceryapp/ui/theme/Theme.kt
+++ GroceryApp/app/src/main/java/com/aniobi/groceryapp/ui/theme/Theme.kt
@@ -2,38 +2,18 @@ package com.aniobi.groceryapp.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = orange,
+    primaryVariant = orange,
+    secondary = orange
 )
 
 @Composable
-fun GroceryAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun GroceryAppTheme(content: @Composable () -> Unit) {
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
